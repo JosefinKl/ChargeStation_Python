ad23e86d59812161079774ec71e57a407ad67eec
josefinkl <josefin.klangefjall@gmail.com>
2026-02-19 17:30:33 +0100

    Prices, both todays and next day. Get input from user when car is needed

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 print("Hej laddvärlden 🚗⚡")
-
+from datetime import datetime
+import pytz
 import charger
 
+sweden = pytz.timezone("Europe/Stockholm")
 
 plug_connected = True
 charge_level = int (input("Write current charging level: "))
+
+while True:
+    try:
+        user_input = input("When do you need the car? (YYYY-MM-DD HH:MM): ")
+        
+        # naive datetime
+        needed_naive = datetime.strptime(user_input, "%Y-%m-%d %H:%M")
+        
+        # Change to swedish time
+        needed_time = sweden.localize(needed_naive)
+        
+        break
+        
+    except ValueError:
+        print("Wrong format! Use YYYY-MM-DD HH:MM")
+
+print("Car needed:", needed_time)
+
 charge_start = input("Write 'Plug in' when plugged in: ")
 
 charge_need = charger.batteryCapacity - charge_level*charger.batteryCapacity/100
@@ -15,5 +35,8 @@ print(time_to_charge)
 if charge_start == "Plug in":
     print("Charger plugged in")
     charger.charging(charge_level, True)
+
+
+
 
 
--- prices.py
+++ prices.py
@@ -1,20 +1,44 @@
 from nordpool import elspot
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 sweden = pytz.timezone("Europe/Stockholm")
 
 prices_spot = elspot.Prices(currency='SEK')
 
-prices = prices_spot.hourly(
-    areas=['SE3'],  #area, choose when registration as user FIX (S1-S4)
-    end_date=datetime.now()
+today = datetime.now()
+tomorrow = today + timedelta(days=1)
+
+# Today's data
+prices_today = prices_spot.hourly(
+    areas=['SE3'], #area, choose when registration as user FIX (S1-S4)
+    end_date=today
 )
 
-for hour in prices['areas']['SE3']['values']:
+# Tomorrow's data
+prices_tomorrow = prices_spot.hourly(
+    areas=['SE3'], #area, choose when registration as user FIX (S1-S4)
+    end_date=tomorrow
+)
+
+# Both lists
+all_hours = (
+    prices_today['areas']['SE3']['values'] +
+    prices_tomorrow['areas']['SE3']['values']
+)
+
+
+now = datetime.now(sweden)
+current_hour = now.replace(minute=0, second=0, microsecond=0)
+
+
+
+for hour in all_hours:
     
     local_time = hour['start'].astimezone(sweden) #Convert from utc to swedish time
+    
+    # Filtrera bort timmar som redan varit
+    if local_time >= current_hour:
+        price_sek_per_kwh = hour['value'] / 1000
+        print(local_time, price_sek_per_kwh)
 
-    price_sek_per_kwh = hour['value'] / 1000
-
-    print(local_time, price_sek_per_kwh)
